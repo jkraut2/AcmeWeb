ba4949068034178c42f012a5cb92cf48d609c340
[jkraut2] <[zevkraut@gmail.com]>
2020-01-12 19:51:27 -0500

    Agile Story #3 - Fixed DiskStatusCommand to not return null result.

--- src/main/java/com/acme/statusmgr/Command/DiskStatusCommand.java
+++ src/main/java/com/acme/statusmgr/Command/DiskStatusCommand.java
@@ -8,27 +8,17 @@ import com.acme.statusmgr.beans.DiskStatus;
     public class DiskStatusCommand implements IDiskStatusCommand
     {
         private DiskStatus result;
-        private long id;
-        private String template;
-        private String name;
 
-        /**
-         *
-         * @param id - command id number
-         * @param template - way the command should look
-         * @param name - name of requester
-         */
+
         public DiskStatusCommand(long id, String template, String name)
         {
-            this.id = id;
-            this.template = template;
-            this.name = name;
+            result = new DiskStatus(id, String.format(template, name));
         }
 
         @Override
         public void execute()
         {
-            result = new DiskStatus(id, String.format(template, name));
+
             result.setDiskCommandOutput(result.generateDiskCommandOutput());
             result.setTimeExecuted(System.currentTimeMillis());
         }
